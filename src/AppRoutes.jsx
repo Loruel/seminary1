1ef2fbb745b799022ab643677f12a1439e3873ee
Loruel <paco.jeansl@gmail.com>
2024-11-15 14:36:09 -0600

    ordenOK

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -9,7 +9,8 @@ import SopaDeLetrasHeroes2 from './pages/SopaHeroes2'
 import SopaDeLetrasBosques2 from './pages/SopaBosques2'
 import SopaDeLetrasCuahu from './pages/SopaCuahu'
 import SopaDeLetrasBosques1 from './pages/SopaBosques1'
-import Crucigrama from './pages/crucigrama'
+import Cruci from './pages/Cruci'
+
 
 export default function AppRoutes() {
     return (
