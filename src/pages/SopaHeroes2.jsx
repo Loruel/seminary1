5c531355320fc15a216013bbc69026c1f2a120af
Loruel <paco.jeansl@gmail.com>
2024-11-15 21:35:05 -0600

    correccion estaca 2

--- src/pages/SopaHeroes2.jsx
+++ src/pages/SopaHeroes2.jsx
@@ -79,7 +79,7 @@ const SopaDeLetrasBosques1 = () => {
     return (
         <div className="max-w-lg mx-auto p-4 text-white h-full" style={{ backgroundColor: '#022044' }}>
             <h1 className="text-2xl font-semibold text-center mb-2 mt-4">Sopa de Letras</h1>
-            <h2 className='text-center text-3xl font-bold mb-4'>HEORES 2</h2>
+            <h2 className='text-center text-3xl font-bold mb-4'>HÉROES 2</h2>
             {mensaje && <div className="text-green-500 font-bold mt-4 text-center mb-4">{mensaje}</div>}
             <div className='flex justify-center'>
                 <div className='mb-6 text-sm w-11/12'>
